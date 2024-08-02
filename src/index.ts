1523068017bd33d0412631e9231191775e6e8d51
Connell <connellsharp@gmail.com>
2024-08-02 15:32:57 +0100

    Move to midi.ts module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { WebMidi, Input } from "webmidi";
 import { createPiano, setNotePressed, setCorrectNotes, onCorrectNotes } from "./virtual-piano";
-import { Note } from "./music-theory";
+import { setupMidiListener } from "./midi";
 import { randomScale, Triad } from "./game";
 
 (function() {
@@ -34,54 +33,5 @@ import { randomScale, Triad } from "./game";
     });
     askForTriad(scale.triads[0]);
 
-    WebMidi.enable()
-    .then(() => {
-        const inputs = WebMidi.inputs;
-
-        if (inputs.length === 0) {
-        console.log("No MIDI input devices detected.");
-        } else {
-        inputs.forEach((input: Input) => {
-            console.log(`Listening to MIDI device: ${input.name}`);
-
-            const addNoteListener = (eventName: "noteon" | "noteoff", on: boolean) => {
-                input.addListener(eventName, (event) => {
-                    var note = (event.note.name + (event.note.accidental ?? "")) as Note;
-                    setNotePressed(note, "all", on);
-                });
-            };
-
-            addNoteListener("noteon", true);
-            addNoteListener("noteoff", false);
-        });
-        }
-    })
-    .catch((err) => console.error("Could not enable WebMidi:", err));
-
-    const noteKeyMap: { [key: string]: Note } = {
-        "z": "C",
-        "s": "C#",
-        "x": "D",
-        "d": "D#",
-        "c": "E",
-        "v": "F",
-        "g": "F#",
-        "b": "G",
-        "h": "G#",
-        "n": "A",
-        "j": "A#",
-        "m": "B",
-    };
-
-    addEventListener("keydown", (event) => {
-        const key = noteKeyMap[event.key];
-        if(key === undefined) return;
-        setNotePressed(key as Note, "all", true);
-    });
-
-    addEventListener("keyup", (event) => {
-        const key = noteKeyMap[event.key];
-        if(key === undefined) return;
-        setNotePressed(key as Note, "all", false);
-    });
-})();
+    setupMidiListener(setNotePressed);
+});
---
+++ src/midi.ts
@@ -0,0 +1,57 @@
+import { WebMidi, Input } from "webmidi";
+import { Note } from "./music-theory";
+
+const setupMidiListener = (onNotePressed: (note: Note, octave: number | "all", pressed: boolean) => void) => {
+    WebMidi.enable()
+        .then(() => {
+            const inputs = WebMidi.inputs;
+
+            if (inputs.length === 0) {
+            console.log("No MIDI input devices detected.");
+            } else {
+            inputs.forEach((input: Input) => {
+                console.log(`Listening to MIDI device: ${input.name}`);
+
+                const addNoteListener = (eventName: "noteon" | "noteoff", on: boolean) => {
+                    input.addListener(eventName, (event) => {
+                        var note = (event.note.name + (event.note.accidental ?? "")) as Note;
+                        onNotePressed(note, "all", on);
+                    });
+                };
+
+                addNoteListener("noteon", true);
+                addNoteListener("noteoff", false);
+            });
+            }
+        })
+        .catch((err) => console.error("Could not enable WebMidi:", err));
+
+    const noteKeyMap: { [key: string]: Note } = {
+        "z": "C",
+        "s": "C#",
+        "x": "D",
+        "d": "D#",
+        "c": "E",
+        "v": "F",
+        "g": "F#",
+        "b": "G",
+        "h": "G#",
+        "n": "A",
+        "j": "A#",
+        "m": "B",
+    };
+
+    addEventListener("keydown", (event) => {
+        const key = noteKeyMap[event.key];
+        if(key === undefined) return;
+        onNotePressed(key as Note, "all", true);
+    });
+
+    addEventListener("keyup", (event) => {
+        const key = noteKeyMap[event.key];
+        if(key === undefined) return;
+        onNotePressed(key as Note, "all", false);
+    });
+};
+
+export { setupMidiListener };
